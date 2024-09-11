22a08d382f3cc41360d53919ce30d666f843d8d2
Manuel Alvarez <manuel.alvarez@pegasi.io>
2024-09-11 12:04:47 -0500

    add enviroments

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Swal from 'sweetalert2'
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:3000/api/v1';
+  private apiUrl = environment.API_URL;
 
   constructor(private http: HttpClient) { }
 
